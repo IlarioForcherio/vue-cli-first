4e027c14153192089dd5af263d0b856a8784b199
ilarioforcherio <ilarioforcherio@gmail.com>
2022-11-22 18:01:58 +0100

    jumbo

--- src/components/HeroJumbotron.vue
+++ src/components/HeroJumbotron.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2>ciao</h2>
+        <h2>JUMBOTRON</h2>
     </div>
 </template>
 
@@ -10,3 +10,23 @@ export default {
  
 }
 </script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.prova{
+    background-color:black;
+}
+</style>
