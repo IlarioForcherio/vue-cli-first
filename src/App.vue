784c8de6efaa15c759609f9767d5a232d5d74d32
ilarioforcherio <ilarioforcherio@gmail.com>
2022-11-22 16:31:12 +0100

    created file

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <AppTitle />
+    <Hero />
+
+  </div>
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppTitle,
+    Hero
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
